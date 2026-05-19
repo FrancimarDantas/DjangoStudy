03d0f276ad3aa9479fa5d058e32ace56d214a5a8
FrancimarAlexandre <francimaralexandredt@gmail.com>
2026-05-19 12:56:17 -0300

    templates atualizados com css

--- BlogSimples/blog/urls.py
+++ BlogSimples/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("profile/<int:id_user>/", views.perfil,name="perfil_user"),
+    path("content/<int:id_post>/", views.visualizar,name="visualizar_conteudo"),
 ]
--- BlogSimples/blog/views.py
+++ BlogSimples/blog/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Postagem
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 def homepage(request):
     postagem = Postagem.objects.all()
-    return render(request,'home.html',{"posta":postagem})
+    return render(request, 'home.html', {"posta": postagem})
+
+
+def perfil(request, id_user):
+    postagem = get_object_or_404(Postagem,autho = id_user)
+    return render(request, 'perfil.html', {"post":postagem})
+
+
+def visualizar(request, id_post):
+    postagem = get_object_or_404(Postagem,autho = id_post)
+    return render(request, 'visualizar.html', {"post":postagem})
